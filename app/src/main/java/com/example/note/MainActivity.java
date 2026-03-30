630ea754ce9b23cd91624ea05286faa0f834d242
Vladsanin777 <vlad.sanin.2007.09@gmail.com>
2026-03-30 22:23:35 +0400

    intermadiate

--- app/src/main/java/com/example/note/MainActivity.java
+++ app/src/main/java/com/example/note/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.note;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
@@ -34,76 +35,73 @@ public class MainActivity extends AppCompatActivity {
         m_notesContext = m_notes.getContext();
     }
 
-    public void addNote(Note data) {
-        LinearLayout note = new LinearLayout(m_notesContext);
-        note.setId(View.generateViewId());
-        note.setLayoutParams(new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT));
-        note.setOrientation(LinearLayout.VERTICAL);
+    public void addNote(View view) {
+        LinearLayout note = createLinearLayoutNote(m_notesContext, LinearLayout.VERTICAL);
 
-        TextView label = new TextView(m_notesContext);
-        label.setId(View.generateViewId());
-        label.setLayoutParams(new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT));
-        label.setTextSize(32);
-        label.setText("label");
-        note.addView(label);
+        m_notes.addView(note);
 
-        Space space = new Space(m_notesContext);
-        space.setLayoutParams(new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                dpToPx(15)));
-        note.addView(space);
+        Context noteContext = note.getContext();
 
-        TextView content = new TextView(m_notesContext);
-        content.setId(View.generateViewId());
-        content.setLayoutParams(new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT));
-        content.setTextSize(24);
-        content.setText("content");
-        note.addView(content);
+        note.addView(createTextView(noteContext, "label", 32));
 
-        LinearLayout footerLayout = new LinearLayout(m_notesContext);
-        LinearLayout.LayoutParams footerParams = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT);
-        footerLayout.setLayoutParams(footerParams);
-        footerLayout.setGravity(Gravity.END);
-        footerLayout.setOrientation(LinearLayout.HORIZONTAL);
+        note.addView(createVertacalSpace(note.getContext(), 15));
 
-        TextView rename1 = createSmallTextView("rename");
-        footerLayout.addView(rename1);
+        note.addView(createTextView(noteContext, "context", 24));
 
-        footerLayout.addView(createHorizontalSpace(12));
-
-        TextView edited1 = createSmallTextView("edited");
-        footerLayout.addView(edited1);
-
-        footerLayout.addView(createHorizontalSpace(12));
-
-        TextView time1 = createSmallTextView("12:00 AM");
-        footerLayout.addView(time1);
+        LinearLayout footerLayout = createLinearLayoutFooter(
+                note.getContext(), LinearLayout.HORIZONTAL);
 
         note.addView(footerLayout);
 
-        setContentView(note);
+        Context footerLayoutContext = footerLayout.getContext();
+
+        footerLayout.addView(createTextView(footerLayoutContext, "rename", 12));
+
+        footerLayout.addView(createHorizontalSpace(footerLayoutContext, 12));
+
+        footerLayout.addView(createTextView(footerLayoutContext, "edited", 12));
+
+        footerLayout.addView(createHorizontalSpace(footerLayoutContext, 12));
+
+        footerLayout.addView(createTextView(footerLayoutContext, "12:00 AM", 12));
     }
 
-    private TextView createSmallTextView(String text) {
-        TextView tv = new TextView(m_notesContext);
+    private LinearLayout createLinearLayoutFooter(Context parent, int orientation) {
+        LinearLayout layout = createLinearLayoutNote(parent, orientation);
+
+        layout.setGravity(Gravity.END);
+
+        return layout;
+    }
+    private LinearLayout createLinearLayoutNote(Context parent, int orientation) {
+        LinearLayout layout = new LinearLayout(parent);
+        layout.setId(View.generateViewId());
+        layout.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT));
+        layout.setOrientation(orientation);
+        return layout;
+    }
+
+    private TextView createTextView(Context parent, String text, int textSize) {
+        TextView tv = new TextView(parent);
         tv.setLayoutParams(new LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.WRAP_CONTENT,
                 LinearLayout.LayoutParams.MATCH_PARENT));
-        tv.setTextSize(12);
+        tv.setTextSize(textSize);
         tv.setText(text);
         return tv;
     }
 
-    private Space createHorizontalSpace(int dp) {
-        Space space = new Space(m_notesContext);
+    private Space createVertacalSpace(Context parent, int dp) {
+        Space space = new Space(parent);
+        int px = dpToPx(dp);
+        space.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, px));
+        return space;
+    }
+
+    private Space createHorizontalSpace(Context parent, int dp) {
+        Space space = new Space(parent);
         int px = dpToPx(dp);
         space.setLayoutParams(new LinearLayout.LayoutParams(px, LinearLayout.LayoutParams.MATCH_PARENT));
         return space;
